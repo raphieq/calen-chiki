73ac09d5e98ffec39c2507af6a684bd207b0e92b
raphieq <raphieq1134@gmail.com>
2015-07-25 11:46:14 +0800

    edit date params
    same

--- app/controllers/calender_controller.rb
+++ app/controllers/calender_controller.rb
@@ -1,8 +1,15 @@
 class CalenderController < ApplicationController
+
   def index
+  	@users = User.all
+  	@users.each do |user|
+  	user.about = user.dob.strftime("%d-%m")
+  	end
+  	@user_by_dob = @users.group_by(&:about)
+
   	@happenings = Happening.all
   	@happenings_by_date = @happenings.group_by(&:date_created)
-  	@date = params[:date] ? Date.parse(params[:date]) : Date.today
 
+  	@date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 end
